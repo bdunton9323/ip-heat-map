9319751ef0ab453737efe169567c8c7f911f5440
Benjamin <benjamin.dunton@gmail.com>
2016-09-24 13:45:47 -0400

    Refactored js code. Fixed a bug where every request from the server was additive (the points would get hotter with every move)

--- heat-map.js
+++ heat-map.js
@@ -7,62 +7,53 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     subdomains: ['a','b','c']
 }).addTo(mymap);
 
-var testPoints = [[35, -78.8986, 0.2], // lat, long, intensity
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -78.8986, 0.2],
-    [35.99, -79, 0.5]];
-
-var heat = null;
-
-$(document).ready(function(){
-    // TODO: do this a better way than having a function initialize a global 'heat' var
-    initHeatLayer();
-    updateView()});
-mymap.on('moveend', updateView);
-//var heat = updateHeatLayer(testPoints);
-
-function initHeatLayer(points) {
-    console.log("Initializing heat layer: " + points);
-    heat = L.heatLayer([], {radius: 30, blur: 15, maxZoom: 20}).addTo(mymap);
-}
-
-function updateHeatLayer(points) {
-    console.log("Updating heat layer: " + points);
-    for (var i = 0; i < points.length; i++) {
-        heat.addLatLng(points[i]);
+var heatMap = {
+    heat: null,
+    points: [],
+    
+    initHeatLayer: function() {
+        console.log("Initializing heat layer: " + this.points);
+        this.heat = L.heatLayer([], {radius: 30, blur: 15, maxZoom: 20}).addTo(mymap);
+    },
+    
+    updateHeatLayer: function(points) {
+        this.points = points;
+        for (var i = 0; i < this.points.length; i++) {
+            this.heat.setLatLngs(this.points);
+        }
     }
 }
 
-function updateView() {
+function updateView(heatMap) {
     var upperLeftLat = mymap.getBounds().getNorth();
     var upperLeftLong = mymap.getBounds().getWest();
     var lowerRightLat = mymap.getBounds().getSouth();
     var lowerRightLong = mymap.getBounds().getEast();
-    
-    console.log("UL: (" + upperLeftLat + ", " + upperLeftLong + ") LR: (" + lowerRightLat + " " + lowerRightLong + ")");
 
     $.ajax({
         type: "GET",
         url: "http://localhost:8888/getdata",
         // TODO: it might be cleaner to pass an array of points: [[x1,y1],[x2,y2]]
         data: {
-            lat1: upperLeftLat, 
+            lat1: upperLeftLat,
             long1: upperLeftLong,
             lat2: lowerRightLat,
             long2: lowerRightLong},
-        //contentType: 'application/json; charset=utf-8',
-        //dataType: "json",
         success: function(response){
-            console.log("got data: " + response.data[0]); 
-            updateHeatLayer(response.data);
+            heatMap.updateHeatLayer(response.data);
         },
         error: function(err){}
     });
 }
+
+$(document).ready(function(){
+    console.log("this is called");
+    heatMap.initHeatLayer();
+    updateView(heatMap);
+    mymap.on('moveend', function(){updateView(heatMap);});
+});
+
+
+
+
 
